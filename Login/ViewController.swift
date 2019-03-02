88e0ab0c179b9fa091e4a3407db76dd27de6ebb9
Erica Bermudez <student9@student9s-macbook-air.local>
2019-03-02 09:41:19 -0600

    Initial Commit

---
+++ Login/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Login
+//
+//  Created by student9 on 3/2/19.
+//  Copyright © 2019 Erica Bermudez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
