e1b5b5448bb3791e5eccc22402d636e7261b4743
Erica Bermudez <student9@student9s-macbook-air.local>
2019-03-02 10:54:08 -0600

    1st commit 3/2/2019 - UI and objects successfully compile

--- Login/ViewController.swift
+++ Login/ViewController.swift
@@ -10,6 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userNameTextField: UITextField!
+    
+    @IBOutlet weak var forgotUserNameButton: UIButton!
+    
+    @IBOutlet weak var forgotPasswordButton: UIButton!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
+    @IBAction func loginButton(_ sender: Any) {
+         performSegue(withIdentifier: "ForgottenUsernameOrPassword", sender: loginButton)
+    }
+    
+    @IBAction func forgotUserNameButton(_ sender: Any) {
+        performSegue(withIdentifier: "ForgottenUsernameOrPassword", sender: forgotUserNameButton)
+    }
+    
+    @IBAction func forgotPasswordButton(_ sender: Any) {
+        performSegue(withIdentifier: "ForgottenUsernameOrPassword", sender: forgotPasswordButton)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        segue.destination.navigationItem.title = userNameTextField.text
+        
+        guard let sender = sender as? UIButton else {return}
+        
+        if sender == forgotPasswordButton {
+            segue.destination.navigationItem.title = "Forgot Password"
+        } else if sender == forgotUserNameButton {
+            segue.destination.navigationItem.title = "Forgot User Name"
+        } else {
+            segue.destination.navigationItem.title = userNameTextField.text
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
